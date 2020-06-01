2d62c2d5b69994ddd53fef8966eda81cb459b405
Nicolas Rios <nickrios33@yahoo.com>
2020-06-01 12:23:33 -0700

    created a . swift file for class contacts

---
+++ Contacts-Swift-Hybrid/Contacts-Hybrid/Contact.swift
@@ -0,0 +1,20 @@
+//
+//  Contact.swift
+//  Contacts-Hybrid
+//
+//  Created by Nicolas Rios on 6/1/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+import Foundation
+
+class contact {
+    var name: String
+    var relationship: String?
+    
+    init(name:String,relationship: String?){
+        self.name = name
+           self.relationship = relationship
+    }
+   
+}
